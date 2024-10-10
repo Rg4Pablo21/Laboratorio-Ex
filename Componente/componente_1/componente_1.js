85d770a52aefd24df3e73d6774731c7f6b448b1a
PG <gic438@gmail.com>
2024-10-10 10:53:20 -0600

    Termiando componente 1

---
+++ Componente/componente_1/componente_1.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <div class="componente">
+        <h2>Componente 1</h2>
+        <div class="descripción">
+          <p>Descripción del componente</p>
+        </div>
+        <div class="caracteristicas">
+          <ul>
+            <li>Característica 1</li>
+            <li>Característica 2</li>
+            <li>Característica 3</li>
+          </ul>
+        </div>
+        <div class="contacto">
+          <input type="text" placeholder="Mensaje">
+          <button>Enviar</button>
+        </div>
+      </div>
+</head>
+<body>
+    
+</body>
+</html>
